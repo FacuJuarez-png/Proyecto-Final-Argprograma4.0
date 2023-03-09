26da2c36a2a35720eabf67205310eca8651cde1f
Facundo Juarez Romero <69168235+FacuJuarez-png@users.noreply.github.com>
2023-03-08 21:05:20 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+document.getElementById('boton').onclick = function () {
+    console.log("informacion desplegada")
+    document.getElementById("demo").innerHTML=  "<li>E-mail: armandoparedesycasas@gmail.com</li> <li>Telefono:123 4567 8900</li> <li> Linkedin:linkedin.com/armandoparedesycasas</li>"
+    
+   
+
+
+    
+    
+    
+    
+};
+/** no me anduvo bien profe :(
+
+document.getElementById('boton2').document.addEventListener('click',function ()   {
+
+    document.body.style.backgroundColor='black';
+
+} )
+*/ 
